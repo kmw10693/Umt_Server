cc032f8f03fb807e366ecafc3ae06b4a5288218f
minu kim <kmw106933@naver.com>
2022-03-17 19:06:53 +0900

    [Feat] 마이 페이지 불러오기 추가

--- src/main/java/com/umt/umt_server/application/UserService.java
+++ src/main/java/com/umt/umt_server/application/UserService.java
@@ -1,23 +1,44 @@
 package com.umt.umt_server.application;
 
 import com.umt.umt_server.domain.User;
+import com.umt.umt_server.dto.User.UserResultData;
+import com.umt.umt_server.infra.SolveRepository;
+import com.umt.umt_server.infra.TreasureRepository;
 import com.umt.umt_server.infra.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
-/**
- * 사용자의 요청을 받아, 실제 내부에서 작동하는 클래스 입니다.
- */
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final SolveRepository solveRepository;
+    private final TreasureRepository treasureRepository;
 
     public User create(User user) {
         return userRepository.save(user);
     }
+
+    public UserResultData getMyPage(Long userId) {
+        Optional<User> user = userRepository.findById(userId);
+
+        Long shareCnt = treasureRepository.findCountByUser(user.get());
+        Long treasureCnt = solveRepository.CountByUser(user.get());
+
+        return UserResultData.builder()
+                .nickname(user.get().getNickname())
+                .imageUrl(user.get().getImageUrl())
+                // TODO 레벨 추가
+                .level(5)
+                .nickname(user.get().getNickname())
+                .treasureShare(shareCnt)
+                .treasureSeen(treasureCnt)
+                .build();
+    }
+
 }
